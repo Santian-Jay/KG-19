5682ea77c6d3bd3f5ee494cc61ab1e329e34c557
Jing Lu <101412798+Santian-Jay@users.noreply.github.com>
2022-05-03 15:34:48 +0800

    Update symmetric.py

--- symmetric.py
+++ symmetric.py
@@ -2,7 +2,9 @@ relationWithHT = {}
 symmetric = {}
 symmetricIndex = 0
 strIndex = []
-
+threshold = 0.95
+total = {}
+newDic = {}
 
 
 file = open("dataset/train2id.txt", "r")
@@ -15,17 +17,21 @@ for index in range(entryNumber):
     content = file.readline()
     head, tile, relation = content.strip().split()
     if relation not in relationWithHT:
+        total[relation] = 0
         strIndex.append(str(relation))
         relationWithHT[relation] = []
+        newDic[relation] = []
     relationWithHT[relation].append((head, tile))
+    newDic[relation].append((head, tile))
 
 
 """
 Symmetric relationship algorithm, print data that can be reversed in each relationship
 """
 
-for i in range(len(relationWithHT)):
-    temp = relationWithHT[strIndex[i]]      # first row
+for i in range(len(newDic)):
+    # print(newDic)
+    temp = newDic[strIndex[i]]      # first row
     temp_1 = [temp[0]]                      # first group of first row
     tempList = [temp_1[0][0], temp_1[0][1]] #  first group of first row change to list
     # print("The first comparison data：", tempList)
@@ -36,6 +42,7 @@ for i in range(len(relationWithHT)):
                 newArray = [temp[j][1], temp[j][0]]
                 # print("The model for comparison is：", newArray)
                 if tempList == newArray:
+                    total[strIndex[i]] += 2
                     symmetric[symmetricIndex] = []
                     symmetric[symmetricIndex].append((strIndex[i], tempList, strIndex[i], (temp[j][0], temp[j][1])))
                     symmetricIndex += 1
@@ -44,5 +51,18 @@ for i in range(len(relationWithHT)):
         # print("update comparative data：", tempList)
         temp.remove(temp[0])
 
+
 for i in range(len(symmetric)):
     print(symmetric[i])
+
+fSymmetric = open("symmetric.txt", "w")
+
+print(relationWithHT)
+print(total)
+for i in range(len(total)):
+    rate = total[strIndex[i]] / len(relationWithHT[strIndex[i]])
+    if rate >= threshold:
+        print("relation %s can be symmetric"%(strIndex[i]))
+        fSymmetric.write("%d\n" % (len(symmetric)))
+        fSymmetric.write("%s\n"%(total[strIndex[i]]))
+
